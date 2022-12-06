c3e280aecd263eae47aecc88879b58a9be118846
Thomas Muzy <muzy.thomas@gmail.com>
2022-12-06 21:16:01 +0100

    forgot part 2 of day 2

--- 02-second/src/main.rs
+++ 02-second/src/main.rs
@@ -1,53 +1,49 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::io::{BufRead, BufReader};
 
-#[derive(Clone)]
-struct WinValue {
-    score: i32,
-    value: i32,
-}
+fn main() {
+    let lines = get_lines("input");
 
-fn main() -> Result<(), Error> {
-    let path = "input";
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
+    let move_scores: HashMap<&str, i32> = [("A", 1), ("B", 2), ("C", 3)].into_iter().collect();
+    let losing_moves: HashMap<&str, &str> =
+        [("A", "C"), ("B", "A"), ("C", "B")].into_iter().collect();
+    let winning_moves: HashMap<&str, &str> =
+        [("A", "B"), ("B", "C"), ("C", "A")].into_iter().collect();
 
-    let scores: HashMap<&str, WinValue> = [
-        ("X", WinValue { score: 1, value: 1 }),
-        ("Y", WinValue { score: 2, value: 2 }),
-        ("Z", WinValue { score: 3, value: 3 }),
-        ("A", WinValue { score: 1, value: 2 }),
-        ("B", WinValue { score: 2, value: 3 }),
-        ("C", WinValue { score: 3, value: 1 }),
-    ]
-    .iter()
-    .cloned()
-    .collect();
-
+    let win_score = 6;
+    let draw_score = 3;
     let mut sum = 0;
-    for line in buffered.lines() {
-        if let Ok(content) = line {
-            if !content.is_empty() {
-                let result = content
-                    .split(" ")
-                    .into_iter()
-                    .collect::<Vec<&str>>()
-                    .into_boxed_slice();
 
-                sum += scores[result[1]].score;
-                if scores[result[1]].value == scores[result[0]].value {
-                    println!("win {:?}", result);
-                    sum += 6
-                } else if scores[result[1]].score == scores[result[0]].score {
-                    println!("draw {:?}", result);
-                    sum += 3
-                } else {
-                    println!("lose {:?}", result);
-                }
+    for line in lines {
+        let moves: Vec<&str> = line.split(" ").collect();
+        print!("{:?}", moves);
+        match moves[1] {
+            "X" => {
+                let strategic_move = losing_moves.get(moves[0]).unwrap();
+                sum += move_scores.get(strategic_move).unwrap();
+                println!("lose {:?} {}", strategic_move, sum);
+            }
+            "Y" => {
+                sum += move_scores.get(moves[0]).unwrap() + draw_score;
+                println!("draw {:?}", sum);
+            }
+            "Z" => {
+                let strategic_move = winning_moves.get(moves[0]).unwrap();
+                sum += move_scores.get(strategic_move).unwrap() + win_score;
+                println!("win {:?} {}", strategic_move, sum);
+            }
+            &_ => {
+                panic!("unexpected");
             }
         }
     }
-    println!("result {}", sum);
-    Ok(())
+    println!("{}", sum)
+}
+
+fn get_lines(filename: &str) -> Vec<String> {
+    BufReader::new(File::open(filename).expect("file not found"))
+        .lines()
+        .map(|l| l.expect("error line read"))
+        .collect()
 }
