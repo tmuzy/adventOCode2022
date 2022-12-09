914c67ba4602f4f200880916c43088a93768a6a7
Thomas Muzy <muzy.thomas@gmail.com>
2022-12-09 23:46:14 +0100

    finished 07

--- 07-seventh/src/main.rs
+++ 07-seventh/src/main.rs
@@ -10,7 +10,7 @@ struct ElfFile {
     name: String,
     size: i64,
     parent: Option<Arc<ElfFile>>,
-    total_size: Cell<u64>,
+    total_size: Cell<i64>,
     depth: usize,
 }
 
@@ -45,77 +45,47 @@ impl ElfFile {
 fn main() {
     let history = get_lines("input");
 
-    let tree_root = Arc::new(ElfFile {
-        name: "/".to_string(),
-        size: -1,
-        parent: None,
-        total_size: Cell::new(0),
-        depth: 0,
-    });
-    let mut file_tree: Vec<Arc<ElfFile>> = vec![tree_root.clone()];
+    let file_tree = build_file_tree(&history);
 
-    let mut current_directory = file_tree[0].clone();
+    print_tree(&file_tree);
 
-    for item in &history[1..] {
-        match get_command_type(item) {
-            HistoryItemType::CD => {
-                if let Some(result) = execute_cd(item, current_directory.clone()) {
-                    current_directory = Arc::new(result);
-                    let already_exists = file_tree.iter().find(|f| {
-                        f.is_dir()
-                            && !f.is_root()
-                            && f.name == current_directory.clone().name
-                            && f.parent.clone().unwrap().name
-                                == current_directory.parent.clone().unwrap().name
-                    });
-                    if !already_exists.is_some() {
-                        file_tree.push(current_directory.clone());
-                    } else {
-                        println!("already exists {}", current_directory.name)
-                    }
-                } else {
-                    current_directory = current_directory.parent.clone().unwrap()
-                }
-            }
-            HistoryItemType::FILE => {
-                if !item.starts_with("dir") {
-                    let mut split_item = item.split_whitespace();
-
-                    let new_file = ElfFile {
-                        size: split_item.next().unwrap().parse::<i64>().unwrap(),
-                        name: split_item.next().unwrap().to_string(),
-                        parent: Some(current_directory.clone()),
-                        total_size: Cell::new(0),
-                        depth: current_directory.depth + 1,
-                    };
-
-                    let file_arc = Arc::new(new_file);
-                    file_tree.push(file_arc.clone());
-                    let mut parent = file_arc.parent.clone().unwrap();
-                    while !parent.is_root() {
-                        parent
-                            .total_size
-                            .set(parent.total_size.get() + file_arc.clone().size as u64);
-                        parent = parent.clone().parent.clone().unwrap()
-                    }
-                }
-            }
-            HistoryItemType::LS => (),
-        }
-    }
-
-    for file in file_tree.iter().filter(|f| f.is_dir()) {
-        if let Some(content) = file.get_content(&file_tree) {
-            print_content(file, &content);
-        }
-    }
     let sum = file_tree
         .iter()
         .filter(|f| f.total_size.get() <= 100000 && f.total_size.get() > 0)
         .map(|f| f.total_size.get())
-        .sum::<u64>();
+        .sum::<i64>();
 
-    println!("sum {}", sum);
+    println!("sum of dirs < 100000 : {}", sum);
+
+    let space_used = file_tree
+        .iter()
+        .find(|f| f.is_root())
+        .unwrap()
+        .total_size
+        .get();
+
+    const SPACE_TOTAL: i64 = 70_000_000;
+    const SPACE_NEEDED: i64 = 30_000_000;
+    println!("free space {}", SPACE_TOTAL - space_used);
+    let space_to_free = SPACE_NEEDED - (SPACE_TOTAL - space_used);
+    println!("space to free : {}", space_to_free);
+
+    let to_remove_size = file_tree
+        .iter()
+        .filter(|f| f.total_size.get() > space_to_free)
+        .map(|f| f.total_size.get())
+        .min()
+        .unwrap();
+
+    println!("dir to delete {}", to_remove_size);
+}
+
+fn print_tree(file_tree: &Vec<Arc<ElfFile>>) {
+    for file in file_tree.iter().filter(|f| f.is_dir()) {
+        if let Some(content) = file.get_content(file_tree) {
+            print_content(file, &content);
+        }
+    }
 }
 
 fn print_content(file: &Arc<ElfFile>, content: &Vec<Arc<ElfFile>>) {
@@ -142,6 +112,64 @@ fn print_content(file: &Arc<ElfFile>, content: &Vec<Arc<ElfFile>>) {
             content_file.size
         );
     }
+}
+
+fn build_file_tree(history: &Vec<String>) -> Vec<Arc<ElfFile>> {
+    let tree_root = Arc::new(ElfFile {
+        name: "/".to_string(),
+        size: -1,
+        parent: None,
+        total_size: Cell::new(0),
+        depth: 0,
+    });
+
+    let mut file_tree: Vec<Arc<ElfFile>> = vec![tree_root.clone()];
+
+    let mut current_directory = file_tree[0].clone();
+
+    for item in &history[1..] {
+        match get_command_type(item) {
+            HistoryItemType::CD => {
+                if let Some(result) = execute_cd(item, current_directory.clone()) {
+                    current_directory = Arc::new(result);
+                    file_tree.push(current_directory.clone());
+                } else {
+                    current_directory = current_directory.parent.clone().unwrap()
+                }
+            }
+            HistoryItemType::FILE => {
+                if !item.starts_with("dir") {
+                    let mut split_item = item.split_whitespace();
+
+                    let new_file = ElfFile {
+                        size: split_item.next().unwrap().parse::<i64>().unwrap(),
+                        name: split_item.next().unwrap().to_string(),
+                        parent: Some(current_directory.clone()),
+                        total_size: Cell::new(0),
+                        depth: current_directory.depth + 1,
+                    };
+
+                    let file_arc = Arc::new(new_file);
+                    file_tree.push(file_arc.clone());
+                    let mut parent = file_arc.parent.clone().unwrap();
+                    while !parent.is_root() {
+                        parent
+                            .total_size
+                            .set(parent.total_size.get() + file_arc.clone().size);
+                        parent = parent.clone().parent.clone().unwrap()
+                    }
+                    if parent.is_root() {
+                        parent
+                            .total_size
+                            .set(parent.total_size.get() + file_arc.clone().size)
+                    }
+                }
+            }
+            HistoryItemType::LS => (),
+        }
+    }
+
+    file_tree
 }
 
 enum HistoryItemType {
