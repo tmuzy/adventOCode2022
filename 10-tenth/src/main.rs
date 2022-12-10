5c8a93bb791c652e813b91385f53dbeb5773c24a
Thomas Muzy <muzy.thomas@gmail.com>
2022-12-10 21:39:34 +0100

    10 part 2

--- 10-tenth/src/main.rs
+++ 10-tenth/src/main.rs
@@ -49,25 +49,45 @@ fn parse_instruction(line: &str) -> Vec<Instruction> {
   }]
 }
 
+fn first_interrupt(x: &isize, cycles: &isize) -> isize {
+  if cycles % 40 == 20 {
+    return x * cycles;
+  }
+  0
+}
+
+fn second_interrupt(x: &isize, row: &isize) {
+  if row == x || row == &(x - 1) || row == &(x + 1) {
+    print!("#");
+  } else {
+    print!(".");
+  }
+}
+
 fn main() {
   let mut cycles: isize = 0;
   let mut x: isize = 1;
   let mut signal_sums = 0;
+  let mut row_i: isize = 0;
   for line in get_lines("input") {
     for instruction in parse_instruction(&line) {
       if let InstructionType::Addx(value) = instruction.t {
         cycles += 1;
-        if cycles % 40 == 20 {
-          println!("interrupt c:{}, x:{} db {}", cycles, x, x * cycles);
-          signal_sums += x * cycles;
-        }
+
+        signal_sums += first_interrupt(&x, &cycles);
+        second_interrupt(&x, &row_i);
         x += value;
       } else {
         cycles += 1;
-        if cycles % 40 == 20 {
-          println!("interrupt c:{}, x:{} db {}", cycles, x, x * cycles);
-          signal_sums += x * cycles;
-        }
+
+        signal_sums += first_interrupt(&x, &cycles);
+        second_interrupt(&x, &row_i);
+      }
+
+      row_i += 1;
+      if cycles % 40 == 0 {
+        row_i = 0;
+        println!("");
       }
     }
   }
